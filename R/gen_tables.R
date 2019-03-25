48951085a5080af1cca40fb710e090409fa57ea2
Will Bonnell <wtb3na@virginia.edu>
2019-03-25 15:56:38 -0500

    updated function to fix the double header as well as filter bad rows scraped

--- R/gen_tables.R
+++ R/gen_tables.R
@@ -22,10 +22,16 @@ gen_tables <- function(year) {
   table_year <- html_table(url_p, header = TRUE, fill = TRUE)
 
 
-  data <- table_year[[1]] #%>%
+  data <- table_year[[1]]
 
-  #add data manip steps here
+  names(data) <- paste(names(data), data[1, ], sep = "_")
+  data <- data[-1,]
+  colnames(data)[1:5] <- c("RK", "Player", "Team", "FantPos", "Age")
+
+  data <- data[!data$RK == "Rk",]
 
   return(data)
 }
+
+
 
